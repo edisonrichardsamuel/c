cfa8c045b4ab09fe62351b865bd126d94b9ce484
edisonrichardsamuel <36623763+edisonrichardsamuel@users.noreply.github.com>
2018-05-25 19:18:14 +0530

    Update sumnatural.c

--- sumnatural.c
+++ sumnatural.c
@@ -4,10 +4,7 @@ int main() {
 	int b,sum=0,i;
  	printf("enter the end value");
 	scanf("%d",&b);
-	for(i=0;i<=b;i++)
-	{
-		sum=sum+i;
-	}
+	sum=n*(n+1))/2;
 	printf("\nsum=%d",sum);
 	return 0;
 }
