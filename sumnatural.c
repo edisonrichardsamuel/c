920923e234d384ebbb220307e1b0f386bc0a06b9
edisonrichardsamuel <36623763+edisonrichardsamuel@users.noreply.github.com>
2018-05-25 19:19:10 +0530

    Update sumnatural.c

--- sumnatural.c
+++ sumnatural.c
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int b,sum=0,i;
+	int b,sum=0;
  	printf("enter the end value");
 	scanf("%d",&b);
-	sum=n*(n+1))/2;
+	sum=(n*(n+1))/2;
 	printf("\nsum=%d",sum);
 	return 0;
 }
