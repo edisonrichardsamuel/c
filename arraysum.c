357119351a89ba6ebdd2a37afda1829979d1e1b2
edisonrichardsamuel <36623763+edisonrichardsamuel@users.noreply.github.com>
2018-06-21 18:51:23 +0530

    Update arraysum.c

--- arraysum.c
+++ arraysum.c
@@ -7,11 +7,11 @@ void main()
     scanf("%d",&n);
     printf("enter element number whose sum should be found:");
     scanf("%d",&k);
-    for(i=0;i<n;i++)
+    for(i=1;i<=n;i++)
     {
         scanf("%d",&a[i]);
     }
-    for(i=0;i<k;i++)
+    for(i=1;i<=k;i++)
     {
         sum=sum+a[i];
     }
