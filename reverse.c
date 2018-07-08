1344dd037bdd8b8839745b956eb7edbf0f71c68e
edisonrichardsamuel <36623763+edisonrichardsamuel@users.noreply.github.com>
2018-07-08 14:07:47 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+int nn;
+    int rem=0;
+		int r=0;
+		while(nn>0)
+		{
+			r=nn%10;
+			nn=nn/10;
+			rem=rem*10+r;
+		}
+		System.out.println(rem);
+    return 0;
+    }
